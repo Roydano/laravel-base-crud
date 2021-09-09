0e32ceb7b01d18ce4a3f190f89025abd5212c5cd
Roydano <roy.dano90@gmail.com>
2021-09-09 18:22:50 +0200

    add seeder e affollato database

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -12,6 +12,22 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach($comics as $comic){
+
+            $newComic = new Comic();
+
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->img = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            $newComic->save();
+
+        }
     }
 }
